c4ec666603c563b241b30805c65f6c68518b23ad
Christian Brown <robrowno@icloud.com>
2022-10-29 14:45:59 +0000

    set DEBUG to False

--- rallyProject/settings.py
+++ rallyProject/settings.py
@@ -39,7 +39,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['rally-blog.herokuapp.com', 'localhost', "*"]
 
